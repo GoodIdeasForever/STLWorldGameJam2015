a5311dcf30812d25b0625a6b2dc63b0a2a167928
Michael Carter <mcarter@simutronics.com>
2015-01-24 13:31:00 -0600

    Created an automatic camera resizer to fix orthogonal camera size when aspect ratio is < 4/3

---
+++ Assets/Scripts/CameraSizer.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSizer : MonoBehaviour
+{
+	Camera myCamera;
+	int screenWidth;
+	int screenHeight;
+
+	void Awake()
+	{
+		myCamera = GetComponent<Camera>();
+		if (null == myCamera)
+		{
+			Debug.LogError("CameraSizer needs to be on a game object with a camera component");
+		}
+	}
+
+	void Update()
+	{
+		if (Screen.width != screenWidth || Screen.height != screenHeight)
+		{
+			RecalculateCameraSize();
+		}
+	}
+
+	void RecalculateCameraSize()
+	{
+		screenWidth = Screen.width;
+		screenHeight = Screen.height;
+
+		// Orthographic size uses screen height, not width, but if our aspect ratio gets way off,
+		// we need to pick a new size based on the effective height to display our full content width
+		if (screenWidth < screenHeight * 4 / 3)
+		{
+			var effectiveHeight = (screenWidth * 3 + 3) / 4;
+			myCamera.orthographicSize = 25 * screenHeight / effectiveHeight;
+		}
+		else
+		{
+			myCamera.orthographicSize = 25;
+		}
+	}
+}
