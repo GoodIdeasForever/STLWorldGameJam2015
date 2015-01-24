4dd13f54a3c5e637f2f9d9173a806b52ff5df440
Charles H Andrus <charles.andrus@orangeinfusion.com>
2015-01-24 15:26:20 -0600

    Updated enemy for movement.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,12 +2,15 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
+    public float tileMovementDuration = 1.0f;
 
     public int X { get; private set; }
     public int Y { get; private set; }
     public Vector2 gridSpacePosition { get { return new Vector2(X, Y); } }
     private bool currentlyMoving = false;
     public Direction FacingDirection { get; private set; }
+    Direction currentMotionDirection;
+    float movementStartTime;
 	// Use this for initialization
 	void Start () 
 	{
@@ -23,13 +26,13 @@ public class Enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-	
-	}
-	void FixedUpdate() 
-	{
         if (!currentlyMoving)
             Move();
 	}
+    void LateUpdate()
+    {
+        AnimateMove();
+    }
     int PrioritizeMoves(Vector2[] moves)
     {
         if (moves.Length.Equals(1))
@@ -65,14 +68,30 @@ public class Enemy : MonoBehaviour {
         Vector2? nextMove = FindMove();
         if (nextMove.HasValue)
         {
+            movementStartTime = Time.time;
+            currentMotionDirection = DirectionExtentions.GetDirection(X, Y, (int)nextMove.Value.x, (int)nextMove.Value.y);
             this.currentlyMoving = true;
-            AnimateMove();
-            GameState.Instance.MoveCharacter(Space.Enemy, X, Y, (int)nextMove.Value.x, (int)nextMove.Value.y);
-            this.currentlyMoving = false;
         }
 	}
 	void AnimateMove()
 	{
-	    
+        if (currentlyMoving)
+        {
+            var t = (Time.time - movementStartTime) / tileMovementDuration;
+            if (t > 1)
+            {
+                var oldGridX = X;
+                var oldGridY = Y;
+                currentlyMoving = false;
+                X += currentMotionDirection.XMotion();
+                Y += currentMotionDirection.YMotion();
+                transform.position = BoardDisplay.Instance.GridToWorldSpace(gridSpacePosition);
+                GameState.Instance.MoveCharacter(Space.Player, oldGridX, oldGridY, X, Y);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(BoardDisplay.Instance.GridToWorldSpace(gridSpacePosition), BoardDisplay.Instance.GridToWorldSpace(gridSpacePosition + currentMotionDirection.WorldSpaceMotion()), t);
+            }
+        }
 	}
 }
