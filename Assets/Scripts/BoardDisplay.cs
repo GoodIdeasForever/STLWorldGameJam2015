f59f2d77cc953b4725541f61c43122fd546baacd
charles_ortman <cto44f@mst.edu>
2015-01-24 12:28:02 -0600

    Added BoardDisplay class and GridToWorldSpace method

---
+++ Assets/Scripts/BoardDisplay.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardDisplay : MonoBehaviour {
+
+    public float spaceHeight = 24f;
+    public float spaceWidth = 32f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public Vector2 GridToWorldSpace(Vector2 gridSpace)
+    {
+        return new Vector2(gridSpace.x * spaceWidth, gridSpace.y * spaceHeight);
+    }
+}
