606676100ae38c49414fb188f34cb71dd7b4d4d9
Michael Carter <mcarter@simutronics.com>
2015-01-24 15:15:55 -0600

    Updated evidence to be a grid placed thing

--- Assets/Scripts/Evidence.cs
+++ Assets/Scripts/Evidence.cs
@@ -1,17 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Evidence : MonoBehaviour {
-    public int X = 0;
-    public int Y = 0;
-
-	// Use this for initialization
-	void Start () {
+public class Evidence : MonoBehaviour
+{
+	public int gridX { get; private set; }
+	public int gridY { get; private set; }
+	public Vector2 gridSpacePosition { get { return new Vector2(gridX, gridY); } }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void SpawnAtGridPosition(int x, int y)
+	{
+		gridX = x;
+		gridY = y;
+		transform.position = BoardDisplay.Instance.GridToWorldSpace(gridSpacePosition);
+		GameState.Instance.PlaceObjectOnBoard(Space.Evidence, gridX, gridY);
 	}
 }
