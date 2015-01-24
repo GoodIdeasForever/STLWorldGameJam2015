25307af7b3b3bbcbe059c078bfa2db5b07da36ae
Michael Carter <mcarter@simutronics.com>
2015-01-24 16:54:49 -0600

    Added a next level tracker mechanic which loads through the pre level scene

---
+++ Assets/Scripts/NextLevelTracker.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NextLevelInfo
+{
+	public string levelName;
+	public int levelNumber;
+}
+
+public class NextLevelTracker : MonoBehaviour
+{
+	public static NextLevelInfo nextLevelInfo;
+
+	public string nextLevelName;
+	public int nextLevelNumber;
+
+	void Awake()
+	{
+		if (!string.IsNullOrEmpty(nextLevelName))
+		{
+			nextLevelInfo = new NextLevelInfo() { levelName = nextLevelName, levelNumber = nextLevelNumber };
+		}
+	}
+
+	public void PlayNextGameLevel()
+	{
+		if (Application.loadedLevelName == "PreLevel")
+		{
+			Application.LoadLevel(nextLevelInfo.levelName);
+		}
+		else
+		{
+			Application.LoadLevel("PreLevel");
+		}
+	}
+}
