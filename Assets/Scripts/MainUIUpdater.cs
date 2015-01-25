f9e09c455fd39746d14c54037843b33e53b945f1
Michael Carter <mcarter@simutronics.com>
2015-01-24 21:38:25 -0600

    Refactoring UI screen to autoupdate.

---
+++ Assets/Scripts/MainUIUpdater.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class MainUIUpdater : MonoBehaviour
+{
+	public Image[] evidenceIcons;
+	public Text timeRemainingText;
+	public Text levelNumber;
+
+	void Update()
+	{
+		for (int i = 0; i < evidenceIcons.Length; ++i)
+		{
+			evidenceIcons[i].enabled = GameState.Instance.Evidence.Count >= (i + 1);
+		}
+
+		timeRemainingText.text = string.Format("{0:2N}:{1:2N}", GameState.Instance.TimeTillEvidenceDrop / 60, GameState.Instance.TimeTillEvidenceDrop % 60);
+	}
+
+	void Start()
+	{
+		levelNumber.text = GameState.Instance.LevelNumber.ToString();
+	}
+}
