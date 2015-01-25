7fe2b6a75b507a6016feb72cd582bb9e243ec816
Michael Carter <mcarter@simutronics.com>
2015-01-25 14:27:55 -0600

    Inventory display hooked up.

--- Assets/Scripts/MainUIUpdater.cs
+++ Assets/Scripts/MainUIUpdater.cs
@@ -10,6 +10,9 @@ public class MainUIUpdater : MonoBehaviour
 	public Text timeRemainingText;
 	public Text levelNumber;
 
+	public Image[] evidenceInventory;
+	public Image[] lootInventory;
+
 	void Awake()
 	{
 		Instance = this;
@@ -39,6 +42,25 @@ public class MainUIUpdater : MonoBehaviour
 		}
 
 		timeRemainingText.text = string.Format("{0:D2}:{1:D2}", (int)(GameState.Instance.NextEvidenceDropTime / 60), (int)(GameState.Instance.NextEvidenceDropTime % 60));
+
+		for (int i = 0; i < evidenceInventory.Length; ++i)
+		{
+			var hasEvidenceInSlot = false;
+			if (GameState.Instance.Player.ItemsInBack.Count > i && GameState.Instance.Player.ItemsInBack[i] == Space.Evidence)
+			{
+				hasEvidenceInSlot = true;
+			}
+			evidenceInventory[i].enabled = hasEvidenceInSlot;
+		}
+		for (int i = 0; i < lootInventory.Length; ++i)
+		{
+			var hasLootInSlot = false;
+			if (GameState.Instance.Player.ItemsInBack.Count > i && GameState.Instance.Player.ItemsInBack[i] == Space.Loot)
+			{
+				hasLootInSlot = true;
+			}
+			lootInventory[i].enabled = hasLootInSlot;
+		}
 	}
 
 	void Start()
