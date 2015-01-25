98709c214916cfdd7a3c9f622977e419e2a7f37b
Michael Carter <mcarter@simutronics.com>
2015-01-25 14:15:03 -0600

    Updated prelevel UI to be informative

--- Assets/Scripts/PreLevel.cs
+++ Assets/Scripts/PreLevel.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class PreLevel : MonoBehaviour
 {
 	public Text levelNumber;
+	public Text levelDescription;
 
 	void Awake()
 	{
@@ -17,7 +18,19 @@ public class PreLevel : MonoBehaviour
 		{
 			yield return null;
 		}
+		yield return null;
 		levelNumber.text = GameState.Instance.LevelNumber.ToString();
+		levelDescription.text = string.Format("Evidence to burn: {0}\n\n" + 
+		                                      "Drop extra evidence every {1} seconds\n\n" +
+		                                      "Dragoncop count: {2}\n\n" +
+		                                      "Can carry {3} item{4}\n\n" +
+		                                      "Loot available: {5}",
+		                                      GameState.Instance.Evidence.Count,
+		                                      GameState.Instance.TimeTillEvidenceDrop,
+		                                      GameObject.FindObjectsOfType<Enemy>().Length,
+		                                      GameState.Instance.MaxNumberOfItemsInPack,
+		                                      GameState.Instance.MaxNumberOfItemsInPack > 1 ? "s" : "",
+		                                      GameState.Instance.Loot.Count);
 	}
 
 	public void StartLevel()
