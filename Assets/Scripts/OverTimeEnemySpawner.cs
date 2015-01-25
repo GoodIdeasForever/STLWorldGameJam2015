caadff59d9d08c39b2975991148dbc8d668af380
Michael Carter <mcarter@simutronics.com>
2015-01-24 18:22:58 -0600

    Added a component to spawn enemies over time.

---
+++ Assets/Scripts/OverTimeEnemySpawner.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class SpawnPosition
+{
+	public int X;
+	public int Y;
+}
+
+public class OverTimeEnemySpawner : MonoBehaviour
+{
+	public Enemy enemyPrefab;
+	public float spawnEveryXSeconds = 10;
+	public SpawnPosition[] spawnPositions = new SpawnPosition[1];
+
+	private float nextEnemySpawnTime;
+
+	void Awake()
+	{
+		nextEnemySpawnTime = Time.time + spawnEveryXSeconds;
+	}
+
+	void Update()
+	{
+		if (Time.time > nextEnemySpawnTime)
+		{
+			nextEnemySpawnTime += spawnEveryXSeconds;
+
+			var newEnemy = GameObject.Instantiate(enemyPrefab) as Enemy;
+
+			var spawnPositionIndex = Random.Range(0, spawnPositions.Length);
+			newEnemy.SpawnAtGridPosition(spawnPositions[spawnPositionIndex].X, spawnPositions[spawnPositionIndex].Y);
+		}
+	}
+}
